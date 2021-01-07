d038ccd316c8232c09f6bba41cc481a7086c7ba0
GROOT <groot.pan@goldendata.cloud>
2021-01-07 16:16:02 +0800

    remove migrate script from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-local}
 
 if [ "$env" != "local" ]; then
-    if [ ! -f '/var/www/html/storage/.init' ]; then
-        (cd /var/www/html && php artisan key:generate && php artisan migrate:refresh --seed && php artisan webstack:clean && touch /var/www/html/storage/.init)
-    fi
     echo "Caching configuration..."
     (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
 fi
